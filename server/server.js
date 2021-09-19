f510201f05173b17866a92e90e13933d7ad98540
SupremeButter <83093090+SupremeButter@users.noreply.github.com>
2021-09-19 13:22:35 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,9 +10,8 @@ var express = require('express')
 var cors = require('cors')
 var app = express()
  
-app.use(cors())
- 
-app.get('/products/:id', function (req, res, next) {
+app.options('/products/:id', cors()) // enable pre-flight request for DELETE request
+app.del('/products/:id', cors(), function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
  
